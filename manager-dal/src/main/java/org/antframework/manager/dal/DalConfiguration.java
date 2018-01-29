926f9835d0f6423121cb431dbb87004af15ca897
zhongxunking <zhongxunking@163.com>
2018-01-30 00:14:33 +0800

    biz common dal web模块允许发布

--- manager-dal/src/main/java/org/antframework/manager/dal/DalConfiguration.java
+++ manager-dal/src/main/java/org/antframework/manager/dal/DalConfiguration.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.domain.EntityScanPackages;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.util.ClassUtils;
@@ -29,10 +30,15 @@ import java.util.List;
 @Configuration
 public class DalConfiguration {
 
+    // jpa扫描配置
+    @Bean
+    public static JpaScanConfiguration jpaScanConfiguration() {
+        return new JpaScanConfiguration();
+    }
+
     /**
      * jpa扫描配置
      */
-    @Configuration
     public static class JpaScanConfiguration implements BeanFactoryAware, BeanDefinitionRegistryPostProcessor, Ordered {
         // Spring的bean工厂
         private BeanFactory beanFactory;
