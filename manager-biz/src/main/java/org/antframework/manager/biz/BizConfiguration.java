abfbde1ac0bce1456532d52e457e171b0ccd44fe
zhongxunking <zhongxunking@163.com>
2018-08-04 01:49:48 +0800

    修复未注册查找管理员服务

--- manager-biz/src/main/java/org/antframework/manager/biz/BizConfiguration.java
+++ manager-biz/src/main/java/org/antframework/manager/biz/BizConfiguration.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.Import;
         AddRelationService.class,
         DeleteManagerService.class,
         DeleteRelationsService.class,
+        FindManagerService.class,
         FindRelationService.class,
         ManagerLoginService.class,
         ModifyManagerNameService.class,
