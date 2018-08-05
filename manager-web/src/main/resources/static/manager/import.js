1bb10ba6f425f5a96dd8e7549c5f5e5dd37d391d
zhongxunking <zhongxunking@163.com>
2018-08-06 00:55:40 +0800

    指定端口为6220、调整import.js

--- manager-web/src/main/resources/static/manager/import.js
+++ manager-web/src/main/resources/static/manager/import.js
@@ -1,26 +1,26 @@
-!function () {
-    // 计算路径
-    let managerPath = function () {
-        let path = document.currentScript ? document.currentScript.src : function () {
-            let scripts = document.scripts;
-            let last = scripts.length - 1;
-            for (let i = last; i >= 0; i--) {
-                if (scripts[i].readyState === 'interactive') {
-                    return scripts[i].src;
-                }
+// 计算根路径
+const managerRootPath = function () {
+    let path = document.currentScript ? document.currentScript.src : function () {
+        let scripts = document.scripts;
+        let last = scripts.length - 1;
+        for (let i = last; i >= 0; i--) {
+            if (scripts[i].readyState === 'interactive') {
+                return scripts[i].src;
             }
-            return scripts[last].src;
-        }();
-        path = path.substring(0, path.lastIndexOf('/'));
-        return path;
+        }
+        return scripts[last].src;
     }();
-    // 导入js、css
-    document.write('<script src="' + managerPath + '/lib/vue.js"></script>');
-    document.write('<script src="' + managerPath + '/lib/vue-router.js"></script>');
-    document.write('<link rel="stylesheet" type="text/css" href="' + managerPath + '/lib/element-ui/element-ui.css">');
-    document.write('<script src="' + managerPath + '/lib/element-ui/element-ui.js"></script>');
-    document.write('<script src="' + managerPath + '/lib/axios.js"></script>');
-    document.write('<script src="' + managerPath + '/lib/qs.js"></script>');
-    document.write('<link rel="stylesheet" type="text/css" href="' + managerPath + '/lib/icon/iconfont.css">');
-    document.write('<script src="' + managerPath + '/common/common.js"></script>');
+    for (let i = 0; i < 2; i++) {
+        path = path.substring(0, path.lastIndexOf('/'));
+    }
+    return path;
 }();
+// 导入js、css
+document.write('<script src="' + managerRootPath + '/manager/lib/vue.js"></script>');
+document.write('<script src="' + managerRootPath + '/manager/lib/vue-router.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="' + managerRootPath + '/manager/lib/element-ui/element-ui.css">');
+document.write('<script src="' + managerRootPath + '/manager/lib/element-ui/element-ui.js"></script>');
+document.write('<script src="' + managerRootPath + '/manager/lib/axios.js"></script>');
+document.write('<script src="' + managerRootPath + '/manager/lib/qs.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="' + managerRootPath + '/manager/lib/icon/iconfont.css">');
+document.write('<script src="' + managerRootPath + '/manager/common/common.js"></script>');
