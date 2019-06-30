5009494eed23cf25f70cfb8fcb73c317b0242b59
zhongxunking <zhongxunking@163.com>
2019-06-30 20:51:14 +0800

    验证签名filter

--- manager-web/src/main/java/org/antframework/manager/web/common/SignFilter.java
+++ manager-web/src/main/java/org/antframework/manager/web/common/SignFilter.java
@@ -19,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -46,33 +48,36 @@ public class SignFilter implements Filter {
     private boolean validateSign(String managerId, String sign, HttpServletRequest request, HttpServletResponse response) throws IOException {
         ManagerInfo manager = Managers.findManager(managerId);
         if (manager == null) {
-            response.getWriter().append(format(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("管理员[%s]不存在", managerId)));
+            write(response, Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("管理员[%s]不存在", managerId));
             return false;
         }
         if (manager.getSecretKey() == null) {
-            response.getWriter().append(format(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("管理员[%s]未设置密钥", managerId)));
+            write(response, Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), String.format("管理员[%s]未设置密钥", managerId));
             return false;
         }
         String parameters = request.getParameterMap().entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
                 .map(entry -> entry.getKey() + "=" + Arrays.stream(entry.getValue()).reduce((left, right) -> left + right).orElse(""))
                 .reduce((left, right) -> left + "," + right)
                 .orElse("");
         parameters += manager.getSecretKey();
         String correctSign = SecurityUtils.digest(parameters);
         if (!Objects.equals(sign, correctSign)) {
-            response.getWriter().append(format(Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), "签名不正确"));
+            write(response, Status.FAIL, CommonResultCode.INVALID_PARAMETER.getCode(), "签名不正确");
             return false;
         }
         ManagerSessionAccessor.setManager(manager);
         return true;
     }
 
-    // 格式化返回结果
-    private String format(Status status, String code, String message) {
-        return String.format("{\"status\":\"%s\",\"code\":\"%s\",\"message\":\"%s\",\"success\":%b}",
+    // 设置返回结果
+    private void write(HttpServletResponse response, Status status, String code, String message) throws IOException {
+        String result = String.format("{\"status\":\"%s\",\"code\":\"%s\",\"message\":\"%s\",\"success\":%b}",
                 status,
                 code,
                 message,
                 status == Status.SUCCESS);
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().append(result);
     }
 }
