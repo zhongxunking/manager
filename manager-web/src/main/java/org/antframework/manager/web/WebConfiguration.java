e15bb24df742caa1c1df3d4c11857ed414a89077
zhongxunking <zhongxunking@163.com>
2021-03-25 19:45:34 +0800

    可配置签名请求的最大存活时间

--- manager-web/src/main/java/org/antframework/manager/web/WebConfiguration.java
+++ manager-web/src/main/java/org/antframework/manager/web/WebConfiguration.java
@@ -8,6 +8,9 @@
  */
 package org.antframework.manager.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.antframework.manager.web.common.GlobalExceptionHandler;
 import org.antframework.manager.web.common.ManagerSignFilter;
 import org.antframework.manager.web.controller.ManagerInitController;
@@ -18,11 +21,16 @@ import org.antframework.manager.web.extension.CurrentManagerService;
 import org.antframework.manager.web.extension.session.ManagerSessionAccessorFilter;
 import org.antframework.manager.web.extension.session.SessionCurrentManagerService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * web层配置
@@ -33,7 +41,12 @@ import org.springframework.core.Ordered;
         ManagerMainController.class,
         ManagerManageController.class,
         RelationManageController.class})
+@EnableConfigurationProperties(WebConfiguration.ManagerProperties.class)
+@AllArgsConstructor
 public class WebConfiguration {
+    // 配置
+    private final ManagerProperties properties;
+
     /**
      * 管理员Session访问器filter
      */
@@ -51,7 +64,7 @@ public class WebConfiguration {
      */
     @Bean
     public FilterRegistrationBean managerSignFilter() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean<>(new ManagerSignFilter(120000));
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean<>(new ManagerSignFilter(properties.getSignedRequest().getMaxSurvivalTime()));
         registrationBean.setOrder(Ordered.LOWEST_PRECEDENCE - 100);
         registrationBean.addUrlPatterns("/*");
 
@@ -62,5 +75,30 @@ public class WebConfiguration {
     @ConditionalOnMissingBean(CurrentManagerService.class)
     @Import(SessionCurrentManagerService.class)
     public static class CurrentManagerConfiguration {
+    }
+
+    /**
+     * 管理员配置
+     */
+    @ConfigurationProperties("ant.manager")
+    @Getter
+    @Setter
+    public static class ManagerProperties {
+        // 签名请求
+        @NotNull
+        private SignedRequest signedRequest = new SignedRequest();
+
+        /**
+         * 签名请求
+         */
+        @Getter
+        @Setter
+        public static class SignedRequest {
+            /**
+             * 选填：签名请求的最大存活时间（单位：毫秒。默认为1分钟）
+             */
+            @Min(0)
+            private long maxSurvivalTime = 60000;
+        }
     }
 }
