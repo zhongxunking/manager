0e34b001016341dd572259f353f625580ec20ea7
zhongxunking <zhongxunking@163.com>
2021-04-11 23:06:24 +0800

    修复无权限误判为未登录，js、css资源版本化

--- manager-web/src/main/resources/static/manager/common/common.js
+++ manager-web/src/main/resources/static/manager/common/common.js
@@ -38,14 +38,19 @@ axios.interceptors.request.use(function (config) {
 // 验证服务端返回结果
 axios.interceptors.response.use(function (response) {
     if (response.status === 200) {
-        var result = response.data;
+        let result = response.data;
         if (result.code === 'common-0004') {
-            // 未登录，则跳转到登录页面
-            Vue.prototype.$confirm('未登录或登录已超时，是否重新登录？', '警告')
-                .then(function () {
-                    window.location.href = MANAGER_LOGIN_PATH;
-                });
-            return Promise.reject(response);
+            // 判断用户是否已登录
+            DO_GET_CURRENT_MANAGER(function (manager) {
+                if (manager === null) {
+                    // 未登录，跳转到登录页面
+                    Vue.prototype.$alert('未登录或登录已超时，请进行登录', '警告', {
+                        callback: function () {
+                            window.location.href = MANAGER_LOGIN_PATH;
+                        }
+                    });
+                }
+            });
         }
         return result;
     } else {
--- manager-web/src/main/resources/static/manager/import.js
+++ manager-web/src/main/resources/static/manager/import.js
@@ -3,27 +3,39 @@ let MANAGER_LOGIN_PATH = null;
 // 当前管理员
 let CURRENT_MANAGER = null;
 // 获取当前管理员
-const GET_CURRENT_MANAGER = function (processor) {
+const GET_CURRENT_MANAGER = function (callback) {
     if (CURRENT_MANAGER !== null) {
-        processor(CURRENT_MANAGER);
+        if (callback) {
+            callback(CURRENT_MANAGER);
+        }
     }
+    DO_GET_CURRENT_MANAGER(function (manager) {
+        if (manager === null) {
+            // 未登录，跳转到登录页面
+            Vue.prototype.$alert('未登录或登录已超时，请进行登录', '警告', {
+                callback: function () {
+                    window.location.href = MANAGER_LOGIN_PATH;
+                }
+            });
+            return;
+        }
+        CURRENT_MANAGER = manager;
+        if (callback) {
+            callback(CURRENT_MANAGER);
+        }
+    });
+};
+// 执行获取当前管理员
+const DO_GET_CURRENT_MANAGER = function (callback) {
     axios.get(MANAGER_ROOT_PATH + '/manager/main/current')
         .then(function (result) {
             if (!result.success) {
                 Vue.prototype.$message.error(result.message);
                 return;
             }
-            if (result.manager === null) {
-                // 未登录，则跳转到登录页面
-                Vue.prototype.$alert('未登录或登录已超时，请进行登录', '警告', {
-                    callback: function () {
-                        window.location.href = MANAGER_LOGIN_PATH;
-                    }
-                });
-                return;
+            if (callback) {
+                callback(result.manager);
             }
-            CURRENT_MANAGER = result.manager;
-            processor(CURRENT_MANAGER);
         });
 };
 // 获取管理员组件的route配置
@@ -55,6 +67,8 @@ const MANAGER_ROOT_PATH = function () {
     }
     return path;
 }();
+
+MANAGER_VERSION = '1.4.6';
 // 是否为开发模式
 const DEV_MODE = function () {
     let host = window.location.hostname;
@@ -62,21 +76,21 @@ const DEV_MODE = function () {
 }();
 // 导入依赖
 if (DEV_MODE) {
-    document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/vue@2.6.10.js"></script>');
+    document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/vue@2.6.10.js?version=' + MANAGER_VERSION + '"></script>');
 } else {
-    document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/vue@2.6.10.min.js"></script>');
+    document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/vue@2.6.10.min.js?version=' + MANAGER_VERSION + '"></script>');
 }
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/vue-router@3.0.7.js"></script>');
-document.write('<link rel="stylesheet" type="text/css" href="' + MANAGER_ROOT_PATH + '/manager/lib/element-ui/element-ui@2.10.1.css">');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/element-ui/element-ui@2.10.1.js"></script>');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/axios@0.19.0.js"></script>');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/qs@6.7.0.js"></script>');
-document.write('<link rel="stylesheet" type="text/css" href="' + MANAGER_ROOT_PATH + '/manager/lib/icon/iconfont.css">');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/common/common.js"></script>');
-document.write('<link rel="stylesheet" type="text/css" href="' + MANAGER_ROOT_PATH + '/manager/common/common.css">');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/vue-router@3.0.7.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="' + MANAGER_ROOT_PATH + '/manager/lib/element-ui/element-ui@2.10.1.css?version=' + MANAGER_VERSION + '">');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/element-ui/element-ui@2.10.1.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/axios@0.19.0.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/lib/qs@6.7.0.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="' + MANAGER_ROOT_PATH + '/manager/lib/icon/iconfont.css?version=' + MANAGER_VERSION + '">');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/common/common.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="' + MANAGER_ROOT_PATH + '/manager/common/common.css?version=' + MANAGER_VERSION + '">');
 // 导入管理员组件
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerLogin.js"></script>');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerInitAdmin.js"></script>');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerManagers.js"></script>');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerProfile.js"></script>');
-document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerMain.js"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerLogin.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerInitAdmin.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerManagers.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerProfile.js?version=' + MANAGER_VERSION + '"></script>');
+document.write('<script src="' + MANAGER_ROOT_PATH + '/manager/components/ManagerMain.js?version=' + MANAGER_VERSION + '"></script>');
